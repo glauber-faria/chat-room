bda5f009e4f23a275f21593a636ade6d72ce53c1
Glauber de Souza Faria <glauber.faria@pti.org.br>
2022-04-14 16:12:29 -0300

    implements react-router-dom

---
+++ front/chat-room-front/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Home from './pages/home';
+import { Route, Routes, Navigate, BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<Navigate to="/home" />}
+        />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
---
+++ front/chat-room-front/src/pages/home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Home() {
+    return(
+        <div>
+            <input></input>
+        </div>
+    );
+}
+
+export default Home;
